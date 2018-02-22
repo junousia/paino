88e7c9e87cf2643d3d0d90d32d4b8a3d0c0c5d37
Jukka Nousiainen <jukka.nousiainen@ericsson.com>
2018-02-22 13:27:40 +0200

    base implementation

--- www/js/paino.js
+++ www/js/paino.js
@@ -1,8 +1,12 @@
 var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1v1gLW2FjISr9x1j-xnTwY1SEvwLOQoLJcocJJm2e25c/pubhtml';
 
-function renderSpreadsheetData() {
+var graph, xAxis, yAxis, axes, hoverDetail, preview, previewXAxis;
+
+var seriesData;
+
+function get_data() {
     Tabletop.init( { key: public_spreadsheet_url,
-                     callback: draw,
+                     callback: updateData,
                      simpleSheet: true,
 					 postProcess: function(element) {
                        // Convert string date into Date date
@@ -14,49 +18,7 @@ function renderSpreadsheetData() {
                  )
 }
 
-function draw(data, tabletop) {
-  // draw chart
-  drawChart(data);
-}
-
-var config = {
-	color1: "red",
-	color2: "red",
-	color3: "red",
-	color4: "red",
-	color5: "red",
-	filter: "Paino",
-	xAxis: "Month",
-	xAxisLabel: "Month",
-	yAxis: [
-		"asdf",
-		"asdf",
-		"asdf",
-		"asdf",
-	],
-	yAxisLabel: "Paino",
-	width: 600,
-	title: "",
-	showLegend: true,
-	height: 400,
-
-}
-
-var innerHtml = $('#canvas-svg').html();
-
-$('#canvas-svg').html(innerHtml);
-
-var loadingSelectAll = true;
-
-function drawChart(chartData, currentFilters) {
-  $('#canvas-svg').html(innerHtml);
-
-  data = chartData;
-
-  var yAxisTypeMap = {};
-  var yAxisType = "";
-
-  var count = 0;
+function updateData(chartData, tabletop) {
   chartData.forEach(function(o) {
     Object.defineProperty(o, 'y',
         Object.getOwnPropertyDescriptor(o, 'Paino'));
@@ -66,80 +28,79 @@ function drawChart(chartData, currentFilters) {
     delete o['Timestamp'];
   });
 
-  seriesData = [
+  data = [
       {
 		name: 'Paino',
         color: 'steelblue',
         data: chartData
       }
   ]
-
-  var margin = {top: 20, left: 20, bottom: 20, right: 20};
-  var width = config.width, height = config.height;
-
-  var graph = new Rickshaw.Graph( {
-      element: $('#canvas-svg').find('.chart-area')[0],
-      width: width - margin.left - margin.right,
-      height: height - margin.top - margin.bottom,
-      min: "auto",
-      renderer: 'line',
-      series: seriesData
-  } );
-
-  graph.render();
-
-  var hoverDetail = new Rickshaw.Graph.HoverDetail(
-	{
-      graph: graph,
-      formatter: function(series, x, y) { return y + " kg" }
-    }
-  );
-
-  var shelving = new Rickshaw.Graph.Behavior.Series.Toggle( {
-      graph: graph
-  } );
-
-  var axes = new Rickshaw.Graph.Axis.Time( {
-      graph: graph
-  } );
-
-  function yFormat(n) {
-    return n + " kg"
-  }
-
-  var yAxis = new Rickshaw.Graph.Axis.Y({
-      graph: graph,
-      orientation: 'left',
-      tickFormat: yFormat,
-      element: $('#canvas-svg').find('.y_axis')[0]
-  });
-  yAxis.render();
-
-  var format = function(n) {
-    if (chartData[n]) {
-      var axisLabel = chartData[n][config.xAxis].toString();
-      if (axisLabel && axisLabel.length < 12) {
-        return axisLabel;
-      } else {
-        return axisLabel.substring(0, 12) + ' ...';
-      }
-    } else {
-      return "";
-    }
-  }
-
-  var xAxis = new Rickshaw.Graph.Axis.X( {
-      graph: graph,
-      orientation: 'bottom',
-      element: $('#canvas-svg').find('.x_axis')[0],
-      pixelsPerTick: 50,
-	  tickFormat: function(x){
-                return new Date(x).toLocaleDateString();
-              }
-  } );
-  xAxis.render();
-
-  axes.render();
+  seriesData = data;
+  draw()
 }
 
-renderSpreadsheetData();
+$(window).on('load', function() {
+	get_data()
+});
+
+function draw() {
+	graph = new Rickshaw.Graph( {
+	    element: document.querySelector(".chart"),
+	    min: "auto",
+		width: 2000,
+		height: 300,
+	    renderer: 'line',
+		interpolation: 'linear',
+		padding: {top: 0.01, left: 0.01, right: 0.01, bottom: 0.01},
+	    series: seriesData
+	} );
+
+	graph.render();
+
+	hoverDetail = new Rickshaw.Graph.HoverDetail(
+	  {
+	    graph: graph,
+	    formatter: function(series, x, y) { return y + " kg" }
+	  }
+	);
+
+	axes = new Rickshaw.Graph.Axis.Time( {
+	    graph: graph,
+		pixelsPerTick: 50,
+	    tickFormat: function(x) { return new Date(x).toLocaleDateString("fi-FI"); }
+	} );
+
+	yAxis = new Rickshaw.Graph.Axis.Y({
+	    graph: graph,
+	    orientation: 'left',
+	    tickFormat: function(y) { return y + " kg" },
+	    element: document.querySelector('.y_axis')
+	});
+
+	xAxis = new Rickshaw.Graph.Axis.X(
+	  {
+	      graph: graph,
+	      orientation: 'bottom',
+	      element: document.querySelector('.x_axis'),
+	      pixelsPerTick: 50,
+	      tickFormat: function(x) { return new Date(x).toLocaleDateString("fi-FI"); }
+	  }
+	);
+
+	xAxis.render();
+	yAxis.render();
+	axes.render();
+
+	var annotator = new Rickshaw.Graph.Annotate({
+	    graph: graph,
+	    element: document.querySelector('.preview')
+	});
+
+	seriesData[0].data.forEach(function(s) {
+	    if (s['Kommentti']) {
+	      annotator.add(s['x'], s['y'] + ' kg: ' + s['Kommentti'])
+	      annotator.update()
+		  console.log(s)
+	    }
+	});
+}
